954ff2a833d3a658aa88504b472cd51049f10d28
Ziv Zamechek <ziv921@yahoo.com>
2016-12-16 17:47:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-const app = "I don't do much."
+function map(collection, callback) {
+  const result = [];
+
+  for (let i = 0; i < collection.length; i++) {
+    const element = collection[i];
+    result.push(callback(element));
+  }
+
+  return result;
+}
+
+const numbers = [1, 2, 3];
+const doubledNumbers = map(numbers, function (number) {
+  return number * 2;
+});
+console.log(doubledNumbers); 
+
+const autobots = [
+  { name: 'Optimus Prime', strength: 5, isTransformed: false, },
+  { name: 'Ironhide', strength: 3, isTransformed: false, },
+  { name: 'Bumblebee', strength: 2.5, isTransformed: false, },
+  { name: 'Ratchet', strength: 1.5, isTransformed: false, },
+];
+
+const transformedAutobots = map(autobots, function (autobot) {
+  return Object.assign({}, autobot, {
+    strength: autobot.strength * 2,
+    isTransformed: true,
+  });
+});
+
+console.log(transformedAutobots);
+
+const transformedAutobotsWithMap = autobots.map(function (autobot) {
+  return Object.assign({}, autobot, {
+    strength: autobot.strength * 2,
+    isTransformed: true,
+  });
+});
+
+console.log(transformedAutobotsWithMap);
